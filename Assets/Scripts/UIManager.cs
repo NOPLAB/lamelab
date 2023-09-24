17b9b128a31c6712a73a6cd9d713150343403662
nop <noplab90@gmail.com>
2023-09-24 18:05:44 +0900

    add audiomixer

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -12,6 +13,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text _volumeValueText;
     [SerializeField] private TMP_Text _volumeIsMuteText;
 
+    [SerializeField] private AudioMixer _audioMixer;
+
     void Start()
     {
         _startButton.onClick.AddListener(OnClickStartButton);
@@ -19,14 +22,23 @@ public class UIManager : MonoBehaviour
         _rankingButton.onClick.AddListener(OnClickRankingButton);
 
         _volumeIsMuteText.enabled = false;
-        _volumeSlider.onValueChanged.AddListener(value =>
-        {
-            _volumeValueText.text = value.ToString();
-            _volumeIsMuteText.enabled = value == 0;
-        });
+        _volumeSlider.onValueChanged.AddListener(OnValueChangedVolumeSlider);
 }
 
     private void OnClickStartButton(){}
     
     private void OnClickRankingButton(){}
+
+    private void OnValueChangedVolumeSlider(float value)
+    {
+        _volumeValueText.text = value.ToString();
+        _volumeIsMuteText.enabled = value == 0;
+        
+        // Normalize
+        value /= 100;
+        //-80~0に変換
+        var volume = Mathf.Clamp(Mathf.Log10(value) * 20f,-80f,0f);
+
+        _audioMixer.SetFloat("Master", volume);
+    }
 }
